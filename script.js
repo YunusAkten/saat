3822d65205c60d833bcbb278768515799e29782b
Yunus Akten <yunusakten2@gmail.com>
2023-09-26 12:23:53 +0300

    kullanıcı adını tutan localstroge eklendi ve yeni kullanıcı için buton eklendi

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-let name = prompt("Adınız nedir?");
-let spanName = document.querySelector("#myName");
-spanName.innerHTML = name[0].toUpperCase() + name.slice(1);
-
+function getName() {
+	let namePrompt = prompt("Adınız nedir?");
+	let spanName = document.querySelector("#myName");
+	let name = namePrompt[0].toUpperCase() + namePrompt.slice(1);
+	spanName.innerHTML = name;
+	localStorage.setItem("name", name);
+}
+if (!localStorage.getItem("name")) {
+	getName();
+} else {
+	let spanName = document.querySelector("#myName");
+	spanName.innerHTML = localStorage.getItem("name");
+}
+let newUser = document.querySelector("#newUser");
+newUser.addEventListener("click", () => {
+	getName();
+});
 function getTime() {
 	let date = new Date();
 	let hour = date.getHours();
@@ -18,7 +31,6 @@ function getTime() {
 	let hourSpan = document.querySelector("#hour");
 	let minuteSpan = document.querySelector("#minute");
 	let secondSpan = document.querySelector("#second");
-	console.log(second);
 	hourSpan.innerHTML = hour;
 	minuteSpan.innerHTML = minute;
 	secondSpan.innerHTML = second;
